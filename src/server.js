7b7c8079a22903212f3578ae4d9903250b9acb67
felipe martins <juninpernadecano@gmail.com>
2024-10-12 15:17:34 -0400

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import express from 'express'
 import sequelize from './models/banco.js'
 import projetos from './routes/projetos.js'
 import email from './routes/email.js'
+import cors from 'cors'
 const app = express()
 const PORT = 8801
 
 app.use(express.json())
+app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 app.use("/portifolio", projetos)
 app.use("/email", email)
 
